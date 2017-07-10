130aada64bd1390661bac081ee271a47ba223c5a
paulsoldan <paulsoldan@gmail.com>
2017-07-10 10:46:13 +0300

    AndroidServer: Modify select username-pasword and select tasks

--- src/servicii/web/Server.java
+++ src/servicii/web/Server.java
@@ -14,28 +14,6 @@ import javax.ws.rs.core.MediaType;
 // in aceasta situatie se va apela una din metodele clasei Hello, in functie de tipul media (text/plain, text/xml sau text/html)
 @Path("/api")
 public class Server {
-
-	// Metoda apelata daca tipul media cerut este TEXT_PLAIN
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayPlainTextHello() {
-		return "Hello Jersey";
-	}
-
-	// Metoda apelata daca tipul media cerut este XML
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	public String sayXMLHello() {
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-	}
-
-	// Metoda apelata daca tipul media cerut este HTML
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
-		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-				+ "<body><h1>" + "Hello Jersey" + "</h1></body>" + "</html> ";
-	}
 	@Path("/checkUser/{username}/{password}")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
